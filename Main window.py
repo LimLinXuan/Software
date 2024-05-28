721d15a0248c173966f43687a779a9a1fcb5befe
LIM LIN XUAN <p22014555@student.newinti.edu.my>
2024-05-28 12:42:01 +0800

    first file

---
+++ Main window.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+root = Tk()
+root.title("First_Program")
+root.geometry("350x200") #(widthxheight)
+
+myLabel = Label(root, text = "Hello World !")
+myLabel.pack()
+
+root.mainloop()
